9f140c526a85134fdc5766c507f7006643ae3e11
jesuscabrita <jesusarnaldo115@gmail.com>
2023-11-29 21:27:20 -0300

    modifi: config

--- server.js
+++ server.js
@@ -10,19 +10,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export const app = express();
 
-// Asegúrate de servir los archivos estáticos correctamente
 app.use(express.static(path.resolve(__dirname, "dist")));
-
 app.use(cors());
 app.use(compression({ brotli: { enabled: true, zlib: {} } }));
 app.use(morgan("dev"));
 
-// Ajusta el orden de las rutas
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
-// Mueve el uso de las rutas después de servir el archivo estático
 plugin_Rutas(app);
 
 const PORT = 5000;
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,16 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+console.log(`
+
+  +------------------------------------------+
+  |                                          |
+    NODE ENVIRONMENT: http://localhost:5000/
+  |                                          |
+  +------------------------------------------+
+
+`)
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.mjs',
+  mode: "development",
+  entry: "./src/index.mjs",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.mjs'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: [".js", ".jsx", ".mjs"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
   },
   module: {
     rules: [
@@ -18,19 +28,29 @@ module.exports = {
         test: /\.(js|mjs|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
+      template: "./public/index.html",
+      filename: "index.html",
     }),
   ],
+  stats: {
+    colors: true,
+    assets: true,
+    modules: false,
+    entrypoints: false,
+    builtAt: true,
+    hash: false,
+    timings: false,
+    version: false,
+  },
 };
